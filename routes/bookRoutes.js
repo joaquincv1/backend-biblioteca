0a6072c1a65255bd2086bc826387f30a22699dca
Joaquin Castillo <joaquinacv04@gmail.com>
2025-11-05 10:18:22 -0300

    Fase 3: Conexión Segura (El Interceptor)

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -7,22 +7,15 @@ const { protect, isAdmin } = require('../middlewares/authMiddleware');
 
 // --- Rutas ---
 
-// Ruta PÚBLICA (Cualquiera puede ver los libros)
-router.get('/', bookController.getAllBooks);
+// ¡Ruta PROTEGIDA! (Solo usuarios logueados pueden ver los libros)
+router.get('/', protect, bookController.getAllBooks);
 
-// Ruta PÚBLICA (Cualquiera puede ver un libro)
-router.get('/:id', bookController.getBookById);
+// ¡Ruta PROTEGIDA!
+router.get('/:id', protect, bookController.getBookById);
 
-// --- Rutas PROTEGIDAS ---
-// Para CREAR un libro, primero debes pasar por el "guardia" (protect)
-// Y ADEMÁS, ser un administrador (isAdmin)
+// --- Rutas de Admin ---
 router.post('/', protect, isAdmin, bookController.createBook);
-
-// Para ACTUALIZAR, igual
 router.put('/:id', protect, isAdmin, bookController.updateBook);
-
-// Para ELIMINAR, igual
 router.delete('/:id', protect, isAdmin, bookController.deleteBook);
-
 
 module.exports = router;
